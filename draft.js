5ff7e2b774053f4be380cf92a0a02a72f9ebc9f6
Vladimir Sychevsky <reactorswins@ukr.net>
2024-05-13 18:27:27 +0300

    ewf

---
+++ draft.js
@@ -0,0 +1,27 @@
+//SSG( Static Site Generation )
+//На сервере храниться кешированный готовый html, данные с API заранее подготовлены при сборке(build)
+//Данный подход идёт по умолчанию(прописивать параметр в запросе не обязательно)
+fetch("https://server.net/api/posts", { cashe: "force-cache" });
+
+//SSR( Server Side Rendering )
+//Перед отправкой клиенту html, сервер опрашивает API и наполняет html страницу контентом
+//Соответственно страница рендериться дольше
+fetch("https://server.net/api/posts", { cashe: "no-store" });
+
+//IRC( Incremental Static Regeneration )
+//На сервере храниться заранее готовая страница
+//Но через свойство revalidate: "значение в секундах", через определённон время актулизирует данные(контент)
+//Тоесть опрашивает API
+fetch("https://server.net/api/posts", { next: { revalidate: 42 } });
+
+//Функция generateStaticParams
+//Используется при динамическом роутинге
+//В файле page.js динамического роута-папки [slug]
+//Название файла, папки и функции должны быть строго такого-же названия
+export async function generateStaticParams() {
+  const posts = await fetch('https://server.net/api/posts').then(res => res.json());
+
+  //id или любой уникальный идентификатор должен быть строковым(логично)
+  return posts.map(post => ({slug: post.id}));
+  //потом в качестве пропса params, этот id доступе через params.id
+}
